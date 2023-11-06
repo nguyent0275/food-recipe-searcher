f1eecec8dfa4ef6b69d7d09f0d0f43003ae51ff3
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-11-05 22:39:07 -0500

    saving previous drinks to past drinks

--- assets/js/cocktaildb.js
+++ assets/js/cocktaildb.js
@@ -3,6 +3,7 @@
 $(function() {
   var requestURL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
   var cocktailListEl = $('#cocktail-list');
+  var pastCocktails = [];
 
   cocktailListEl.hide();
 
@@ -23,6 +24,7 @@ $(function() {
     console.log(jsonData);
     console.log(jsonData.drinks[0].strDrink);
     cocktailListEl.text("")
+    
     if (jsonData.drinks) {
       var drinks = jsonData.drinks;
 
@@ -30,50 +32,63 @@ $(function() {
           var drink = drinks[i];
           var drinkName = drink.strDrink;
           var drinkInstructions = drink.strInstructions;
+          var drinkThumbnail = drink.strDrinkThumb;
 
           var li = $('<li>');
           var imgEl = $('<img>');
           imgEl.attr({
             alt: "",
-            src: drink.strDrinkThumb
+            src: drinkThumbnail
           })
 
           
           li.append(imgEl);
-
           li.append(drinkName);
           cocktailListEl.append(li);
 
           var p = $('<p>');
           p.text(drinkInstructions);
           cocktailListEl.append(p);
+
+          pastCocktails.push({
+            name: drinkName,
+            instructions: drinkInstructions,
+            thumbnail: drinkThumbnail
+          });
       }
   }
 
+    displayPastCocktails();
+  }
+
+  function displayPastCocktails() {
+    var pastCocktailsContainer = $('#past-cocktails');
+    pastCocktailsContainer.empty();
+
+    for (var i = 0; i < pastCocktailsContainer.length; i++) {
+      var pastCocktail = pastCocktails[i];
+
+      var pastCocktailDiv = $('<div>');
+      var pastCocktailName = $('<h2>');
+      pastCocktailName.text(pastCocktail.name);
+
+      var pastCocktailThumbnail = $('<img>');
+      pastCocktailThumbnail.attr({
+        alt: "",
+        src: pastCocktail.thumbnail
+      });
+      
+      var pastCocktailInstructions = $('<p>');
+      pastCocktailInstructions.text(pastCocktail.instructions);
+
+      pastCocktailDiv.append(pastCocktailName);
+      pastCocktailDiv.append(pastCocktailThumbnail);
+      pastCocktailDiv.append(pastCocktailInstructions);
+
+      pastCocktailsContainer.append(pastCocktailDiv);
+    }
   }
 });
-
-// function firstFetch(user) {
-//   var requestURL = 'https://api.github.com/users/' + user;
-//   var cocktailListEl = $('#cocktail-List');
-
-//   $.get(requestURL, function (data) {
-//     console.log(data);
-//     var image = data.avatar_url;
-//     var repoUrl = data.repos_url;
-
-//     var imgEl = $('<img>');
-
-//     imgEl.attr({
-//       alt: "avatar",
-//       src: image 
-//     });
-
-//     cocktailListEl.append(imgEl);
-
-//     reposFetch(repoUrl);
-//   });
-// }
 
 function reposFetch(url) {
   var cocktailListEl = $('#cocktail-list');
